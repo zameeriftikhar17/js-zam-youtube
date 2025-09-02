edd4d4d579d28304201586cd6f388158e8cb3679
zameeriftikhar17 <zameeriftikhar17@gmail.com>
2025-09-02 11:50:11 +0000

    Conversion Operators

---
+++ 01_basics/03_ConversionOperators.js
@@ -0,0 +1,12 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
