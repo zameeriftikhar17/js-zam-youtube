0d0127caa07f7a6329abe5804b3601f7d4d1da8f
zameeriftikhar17 <zameeriftikhar17@gmail.com>
2025-09-02 12:06:52 +0000

    comparisions

---
+++ 01_basics/04_Comparisions.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+
+console.log("3" > 1);
+console.log("03" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+
+
+
+// console.log(undefiend > 0);
+
+console.log("2" === 2);
