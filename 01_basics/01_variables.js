a6fa834f4d218fa1f4553a1924c3b8e33d521ef2
zameeriftikhar17 <zameeriftikhar17@gmail.com>
2025-09-02 11:36:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12356
+let accountEmail = "zam@gmail.com"
+var accountPassword ="12345"
+accountCity = "Sialkot"
+
+// accountId = 2 
+accountEmail = "abc@gmail.com"
+accountPassword ="999"
+accountCity = "lahore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log(3+3)
+
+console.log("zameer")
+
+let name = "zameer"
+let age =21
+let isLoggedIn = false
+let state;
+
+console.log(typeof age)
