8730ddc32c269cfb9388426219d22cfcd4c22aef
zameeriftikhar17 <zameeriftikhar17@gmail.com>
2025-09-03 05:14:45 +0000

    DataType Summary

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types: string, Number, Boolean, null, undefiend, sybmol, BigInt
+//Examples:
+const score = 100
+const scoreValue = 100.6
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id==anotherId);
+
+const bigNumber = 123456789
+
+// Non-primitive or Reference
+
+// Array, Objects, Functions
+
+const heros = ["Ali, Ahmed"]
+let myObj 
+{
+    name: "Zameer";
+    age: 21;
+}
+
+const myFunction = function()
+{
+    console.log("helloooo");
+}
+
+console.log(typeof myFunction);
