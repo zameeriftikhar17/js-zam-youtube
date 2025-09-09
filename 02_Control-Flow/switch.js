dcc2f6f3bf03e7f69e25c31c79d2a1bc62e4f306
zameeriftikhar17 <zameeriftikhar17@gmail.com>
2025-09-09 10:38:51 +0000

    switch

---
+++ 02_Control-Flow/switch.js
@@ -0,0 +1,28 @@
+//switch (key) 
+//{
+    // case value:
+
+    // break;
+
+    // deafult:
+    // break;
+//}
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+case 2:
+        console.log("feb");
+        break;
+        case 3:
+        console.log("march");
+        break;
+        case 4:
+        console.log("april");
+        break;
+    default:
+        break;
+}
