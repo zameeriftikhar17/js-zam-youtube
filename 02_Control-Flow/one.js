ce07638cb1a8e05e2f3278dad56d0cae5fb83892
zameeriftikhar17 <zameeriftikhar17@gmail.com>
2025-09-09 10:31:28 +0000

    control flow file one

---
+++ 02_Control-Flow/one.js
@@ -0,0 +1,57 @@
+//if
+
+/*const temperature =41
+
+if(temperature < 50)
+{
+  console.log("less than 50");
+}
+else{
+    console.log("temperature is greater than 50");
+}
+console.log("executed");*/
+
+
+/*const score = 200
+
+if(score>100)
+{
+    const power = "fly"
+    console.log(`User Power: ${power}`);
+}
+console.log(`User Power: ${power}`);*/
+
+
+/*const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+
+if(balance < 500)
+{
+    console.log("less than 500")
+} else if (balance < 750)
+{
+    console.log("less than 750")
+} else if (balance < 900)
+{
+    console.log("less than 750")
+}
+else
+{
+    console.log("less than 1200")
+} */
+
+    const userLoggedIn = true
+    const debitCard = true
+    const loggedInFromGoogle = false
+    const loggedInfromEmail = true
+    
+    if(userLoggedIn && debitCard && 2==3)
+    {
+        console.log("Allow to buy course")
+    }
+
+    if(loggedInFromGoogle || loggedInfromEmail)
+    {
+        console.log("User Logged In");
+    }
